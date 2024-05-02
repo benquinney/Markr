af1c79a26222f2b2204b7041df5fcaddb5cf8e8b
Ben Quinney <ben.quinney@gmail.com>
2024-05-02 11:30:10 +1000

    Very basic data import and results working

--- app/import/index.php
+++ app/import/index.php
@@ -1,25 +1,55 @@
 <?php
 $data_directory = '/var/www/html/data';
 
+$json_response = array(
+    'result' => 'fail',
+    'message' => 'No XML data provided',
+);
+
 // Accept the XML input
 $dataPOST = trim(file_get_contents('php://input'));
 if(!empty($dataPOST))
 {
-    $xmlData = simplexml_load_string($dataPOST);
+    $xml = simplexml_load_string($dataPOST);
+    $xml_array = json_decode(json_encode((array)$xml), true);
+
+    $students = array();
     
-    foreach($xmlData->children() as $result)
+    foreach($xml_array['mcq-test-result'] as $result)
     {
-        $student_number = $result->{'student-number'};
-        $test_id = $result->{'test-id'};
+        $student_number = $result['student-number'];
+        $test_id = $result['test-id'];
+        $marks_obtained = $result['summary-marks']['@attributes']['obtained'];
+        $saved_results = array(
+            "student_number" => $student_number,
+            "test_id" => $test_id,
+            "marks_obtained" => $marks_obtained,
+        );
+
+        if(empty($students[$student_number]))
+        {
+            $students[$student_number] = 1;
+        }
+        else
+        {
+            $students[$student_number] = $students[$student_number] + 1;
+        }
         
         if(!is_dir($data_directory.'/'.$test_id))
         {
             mkdir($data_directory.'/'.$test_id);
         }
 
-        $f = fopen($data_directory.'/'.$test_id.'/'.$student_number.'.xml', 'wb');
-        fputs($f, $dataPOST);
+        $f = fopen($data_directory.'/'.$test_id.'/'.$student_number.'-'.date("Ymdhis").'-'.$students[$student_number].'.txt', 'wb');
+        fputs($f, json_encode($saved_results));
         fclose($f);
     }
+
+    $json_response = array(
+        'result' => 'success',
+        'message' => 'XML data imported',
+    );
 }
+
+echo json_encode($json_response);
 ?>
---
+++ app/results/index.php
@@ -0,0 +1,100 @@
+<?php
+$data_directory = '/var/www/html/data';
+
+$json_response = array(
+    'result' => 'fail',
+    'message' => 'Action not found',
+    'data' => '',
+);
+
+$test_results_array = array();
+
+$test_id = $_GET['test_id'];
+$action = $_GET['action'];
+
+if($action == 'aggregate')
+{
+    $json_response = array(
+        'result' => 'fail',
+        'message' => 'No results found',
+        'data' => '',
+    );
+
+    if(!is_dir($data_directory.'/'.$test_id))
+    {
+        echo json_encode($json_response);
+        die();
+    }
+    
+    $test_results = scandir($data_directory.'/'.$test_id);
+    foreach($test_results as $test_result)
+    {
+        if($test_result != '.' && $test_result != '..')
+        {
+            $file_content = file_get_contents($data_directory.'/'.$test_id.'/'.$test_result);
+            $student_result = json_decode($file_content, true);
+    
+            if(empty($test_results_array[$student_result['student_number']]))
+            {
+                $test_results_array[$student_result['student_number']] = $student_result['marks_obtained'];
+            }
+            else
+            {
+                if($student_result['marks_obtained'] > $test_results_array[$student_result['student_number']])
+                {
+                    $test_results_array[$student_result['student_number']] = $student_result['marks_obtained'];
+                }
+            }
+        }
+    }
+    sort($test_results_array);
+    
+    if(count($test_results_array) > 0)
+    {
+        $result_data = array(
+            'mean' => 0,
+            'min' => 0,
+            'max' => 0,
+            'p25' => 0,
+            'p50' => 0,
+            'p75' => 0,
+            'count' => count($test_results_array),
+        );
+
+        $score_total = 0;
+        foreach($test_results_array as $tra)
+        {
+            if($tra < $result_data['min'])
+            {
+                $result_data['min'] = $tra;
+            }
+
+            if($tra > $result_data['max'])
+            {
+                $result_data['max'] = $tra;
+            }
+
+            $score_total += $tra;
+        }
+
+        $result_data['mean'] = $score_total / count($test_results_array);
+
+        $p25_location = round(count($test_results_array) * 0.25, 0) - 1;
+        $result_data['p25'] = $test_results_array[$p25_location];
+
+        $p50_location = round(count($test_results_array) * 0.5, 0) - 1;
+        $result_data['p50'] = $test_results_array[$p25_location];
+
+        $p75_location = round(count($test_results_array) * 0.75, 0) - 1;
+        $result_data['p75'] = $test_results_array[$p75_location];
+
+        $json_response = array(
+            'result' => 'success',
+            'message' => 'Results found',
+            'data' => $result_data,
+        );
+    }
+}
+
+echo json_encode($json_response);
+?>
